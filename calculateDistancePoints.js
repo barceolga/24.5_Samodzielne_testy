651d050953ad32b49bb5595e979d53d95dc426e9
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-08-01 16:56:29 +0200

    Add function for calculating points for distance in ski jumping

--- calculateDistancePoints.js
+++ calculateDistancePoints.js
@@ -1,6 +1,14 @@
 // calculateDistancePoints.js
 const calculateDistancePoints = (distance, hillSize, kPoint) => {
-  
+  let totalDistancePoints;
+  if (hillSize == 'normal' ){
+    totalDistancePoints = 60 + (2 * (distance - kPoint));
+  } else if (hillSize == 'big') {
+    totalDistancePoints = 60 + (1.8 * (distance - kPoint));
+  } else if (hillSize == 'mammoth') {
+    totalDistancePoints = 120 + (1.2 * (distance - kPoint));
+  }
+  return totalDistancePoints;
 };
 
 module.exports = calculateDistancePoints;
