caa15eea4680b1ea9b533bba478ef83384284748
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-08-02 07:35:37 +0200

    Update calculateDistancePoints.js file with switch statement

--- calculateDistancePoints.js
+++ calculateDistancePoints.js
@@ -1,14 +1,23 @@
 // calculateDistancePoints.js
 const calculateDistancePoints = (distance, hillSize, kPoint) => {
   let totalDistancePoints;
-  if (hillSize == 'normal' ){
-    totalDistancePoints = 60 + (2 * (distance - kPoint));
-  } else if (hillSize == 'big') {
-    totalDistancePoints = 60 + (1.8 * (distance - kPoint));
-  } else if (hillSize == 'mammoth') {
-    totalDistancePoints = 120 + (1.2 * (distance - kPoint));
+  switch(true) {
+    case (hillSize == 'normal'):
+      totalDistancePoints = 60 + (2 * (distance - kPoint));
+      return totalDistancePoints;
+    break;
+    case (hillSize == 'big'):
+      totalDistancePoints = 60 + (1.8 * (distance - kPoint));
+      return totalDistancePoints;
+    break;
+    case (hillSize == 'mammoth'):
+      totalDistancePoints = 120 + (1.2 * (distance - kPoint));
+      return totalDistancePoints;
+    break;
+    default:
+      return 'We do not have any other type of hill';
+    break;
   }
-  return totalDistancePoints;
 };
 
 module.exports = calculateDistancePoints;
